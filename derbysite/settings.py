83805b43213eea704c458fa21e77f2cd352b1e85
Alan Smith <alan@randomsmiths.com>
2021-01-16 12:48:50 -0500

    Secret key now comes from env

--- derbysite/settings.py
+++ derbysite/settings.py
@@ -19,9 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=dl-xglu!tbq5)e&fhxd*nvl&jnow%$!pafkx+0ocy4g!))bgg'
+import os, random, string
 
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', ''.join(random.choices(string.ascii_uppercase + string.digits, k=20)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
