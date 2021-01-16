ddf4855d191e0a4e83e321f34003cc36274e149f
Alan Smith <alan@randomsmiths.com>
2021-01-16 12:50:50 -0500

    Made it use derby for blank

--- derbysite/urls.py
+++ derbysite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('derby.urls')),
     path('derby/', include('derby.urls')),
     path('admin/', admin.site.urls),
 ]
