b901bb165aa522eb103e21c06d417dfb76be9632
Alan Smith <alan@randomsmiths.com>
2021-02-07 13:55:28 -0500

    Add to admin

--- derby/admin.py
+++ derby/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Group, Car, Heat, Result
+
+admin.site.register(Group)
+admin.site.register(Car)
+admin.site.register(Heat)
+admin.site.register(Result)
