b7f2f0f77dd1f174e5081c43eabb2e9bfb1979d0
Alan Smith <alan@randomsmiths.com>
2021-02-07 13:40:10 -0500

    Changed to use redirect

--- derbysite/urls.py
+++ derbysite/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('', include('derby.urls')),
+    path('', RedirectView.as_view(url='derby/')),
     path('derby/', include('derby.urls')),
     path('admin/', admin.site.urls),
 ]
