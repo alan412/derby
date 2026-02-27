ce80ca120fe08aaffc83dc39dd9462d2a186041c
Alan Smith <alan@randomsmiths.com>
2026-02-27 15:22:54 -0500

    Make to use webcam

--- derby/forms.py
+++ derby/forms.py
@@ -1,5 +1,19 @@
+from django import forms
 from django.forms import ModelForm, Form, ModelChoiceField
 from derby.models import Car, Group
+
+
+class CameraImageInput(forms.ClearableFileInput):
+    template_name = 'django/forms/widgets/clearable_file_input.html'
+    
+    def __init__(self, attrs=None):
+        default_attrs = {
+            'accept': 'image/*',
+            'capture': 'environment',  # Use rear camera on mobile devices
+        }
+        if attrs:
+            default_attrs.update(attrs)
+        super().__init__(attrs=default_attrs)
 
 
 class RegisterForm(ModelForm):
@@ -8,4 +22,7 @@ class RegisterForm(ModelForm):
         fields = ['owner', 'name', 'group', 'picture']
         labels = {
             'name': 'Car Name',
+        }
+        widgets = {
+            'picture': CameraImageInput(),
         }
